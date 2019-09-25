15fb5748042594273062a347e341d77778f31be1
Dilovar <impala7691@gmail.com>
2019-09-25 11:03:35 +0500

    a lot of changes

--- js/main.js
+++ js/main.js
@@ -300,51 +300,21 @@ $(document).ready(function() {
   });
 });
 
-/// typeWriter
-
-$(document).ready(function(event) {
-  // array with texts to type in typewriter
-  var dataText = ["Ёвар - качество, которого ждали!"];
-
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    if (i < text.length) {
-      // add next character to h1
-      document.querySelector(".main-text").innerHTML =
-        text.substring(0, i + 1) +
-        '<span class="ss" aria-hidden="true"></span>';
-
-      // wait for a while and call this function again for next character
-      setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback);
-      }, 100);
-    }
-    // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == "function") {
-      // call callback after timeout
-      setTimeout(fnCallback, 700);
-    }
-  }
-  // start a typewriter animation for a text in the dataText array
-  function StartTextAnimation(i) {
-    /*
-    if (typeof dataText[i] == "undefined") {
-      setTimeout(function() {
-        StartTextAnimation(0);
-      }, 20000);
-    }
-    */
-    // check if dataText[i] exists
-    if (i < dataText[i].length) {
-      // text exists! start typewriter animation
-      typeWriter(dataText[i], 0, function() {
-        // after callback (and whole text has been animated), start next text
-        StartTextAnimation(i + 1);
-      });
-    }
-  }
-  // start the text animation
-  StartTextAnimation(0);
+$(document).ready(function() {
+  $("#owl-demo").owlCarousel({
+    nav: true, // Show next and prev buttons
+    navText: [
+      "<i class='sliderbtn fa fa-caret-left'></i>",
+      "<i class='sliderbtn fa fa-caret-right'></i>"
+    ],
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    loop: true,
+    autoplay: true,
+    items: 1,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsMobile: false
+  });
 });
